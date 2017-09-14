7001c8b699ab3f7468a645f597ddc3bec2951f14
Joshua Efe <joshuaefe@TsmnbxRemix.local>
2017-09-14 13:49:15 -0400

    added Tsmnbx Element

---
+++ Tytos_Algorithm.py
@@ -0,0 +1,23 @@
+while True:
+    First_word = input("Type in your first word or name:")
+    Second_word = input("Type in your second word or name:")
+    Third_word = input("Type in your third word or name:")
+
+    def tsmnbx(x,y,z):
+        t = x[0]
+        s = x[-1]
+        m = y[0]
+        n = y[-1]
+        b= z[0]
+        x = z[-1]
+
+        return t + s + m + n + b + x
+
+    def ytoonsoo(x,y,z):
+        pass
+
+
+    print(tsmnbx(First_word, Second_word, Third_word))
+
+    w = input("would you like to try another?:")
+    print("I don't Actaully care we will play again")
